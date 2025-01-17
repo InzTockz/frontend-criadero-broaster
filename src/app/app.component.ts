b2d9ef7509ed9c79b5e54b344ea2d935469c2513
Fredy <inztockz@gmail.com>
2025-01-17 16:55:52 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+// import function to register Swiper custom elements
+import { register, SwiperContainer } from 'swiper/element/bundle';
+import { SwiperOptions } from 'swiper/types';
+// register Swiper custom elements
+register();
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    FormsModule, 
+    ReactiveFormsModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'frontend-pollitos';
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    
+  }
 }
